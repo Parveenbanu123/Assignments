380a47b2f511815c421adb507936b6b78e7e41e8
Parveenbanu123 <parveenbanu.s0210@gmail.com>
2025-12-01 17:15:55 +0530

    webapp1 commit

---
+++ MavenProject9_WebProject1/src/main/java/com/mphasis/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.mphasis;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		String username=request.getParameter("uname");
+		String pwd=request.getParameter("pwd");
+		//out.println("Login Successfull for user "+username);
+		
+		if(username.equals("Parveen") && pwd.equals("123")) {
+			out.println(username+" authenticated successfully");
+		}
+		else {
+			out.println("Login Failed");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ MavenProject9_WebProject1/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2><%="Login From" %></h2>
+<form action="LoginServlet" method="get">
+<label>User Name:</label>
+<input type="text" name="uname">
+<label>Password:</label>
+<input type="password" name="pwd">
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
