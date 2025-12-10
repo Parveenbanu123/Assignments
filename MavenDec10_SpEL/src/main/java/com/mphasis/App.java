6a49d14590fd7a8280adeba8331d73fe1c69eae6
Parveenbanu123 <parveenbanu.s0210@gmail.com>
2025-12-10 17:29:16 +0530

    SpEL commit

---
+++ MavenDec10_SpEL/src/main/java/com/mphasis/App.java
@@ -0,0 +1,20 @@
+package com.mphasis;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.mphasis.beans.Order;
+import com.mphasis.config.ConfigClass;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(ConfigClass.class);
+		Order o=(Order) context.getBean("order");
+		System.out.println(o);
+		//o.getItemList().forEach(System.out::println);
+		o.getItems();
+		context.close();
+    }
+}
